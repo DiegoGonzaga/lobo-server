c17924403da75f87aac49928a9511cddc6075ee6
Diego de Melo Gonzaga <diego-gonzaga@live.com>
2019-06-06 11:27:08 -0300

    Adicinado function 'PreencherString'. Melhorado 'utilitario.horario' para que os números sempre contenham dois dígitos.

--- src/extras/utils.js
+++ src/extras/utils.js
@@ -1,14 +1,52 @@
 const utilitario = {};
+const SIDE=
+{
+    LEFT:1, RIGHT:2
+}
 
 utilitario.isNumber = (n)=>
 {
     return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
+/*
+ * Mostra o horário atual entre colchetes
+ */
 utilitario.horario = ()=>
 {
     const d = new Date();
-    const h = "["+d.getHours()+":"+ d.getMinutes() +":"+ d.getSeconds()+"]";
+    const hora=preecheString(d.getHours(),'0',SIDE.LEFT,2);
+    const minuto=preecheString(d.getMinutes(),'0',SIDE.LEFT,2);
+    const segundo=preecheString(d.getSeconds(),'0',SIDE.LEFT,2);
+    const h = "["+hora+":"+ minuto +":"+ segundo+"]";
     return h;
+}
+
+/*
+ * @param word - palavra a ser preenchida 
+ * @param character - caracter que será incluído
+ * @param side - qual será o lado que o caracter será incluído
+ * @param size - tamanho final da String final
+ * @return String - retorna uma String com o tamanho definido de 'size'
+ */
+const preecheString =(word,character,side,size)=>
+{
+    let nova=word+'';
+    if (side == SIDE.LEFT) 
+    {
+        while (nova.length < size)
+            nova = character + nova;
+    }
+    else if (side == SIDE.RIGHT) 
+    {
+        while (nova.length < size)
+            nova = nova + character;
+    }
+    else
+    {
+        nova='';
+    }
+        
+    return nova;
 }
 export default utilitario;

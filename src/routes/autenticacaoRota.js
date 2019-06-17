c32596642f360ef9bbb2c131c2b23cc3763c2f1d
Diego de Melo Gonzaga <diego-gonzaga@live.com>
2019-06-17 10:07:31 -0300

    Adicionado forma de autenticação no servidor. Adicionado rota para Incidentes.

--- src/routes/autenticacaoRota.js
+++ src/routes/autenticacaoRota.js
@@ -4,4 +4,5 @@ export default function rotearAutenticacaoRota(router)
 {
     router.get('/gerarToken',assinaToken);
     router.get('/verificarToken',checkToken,(req,res)=>{res.send({auth:true,message:"Token is valid."})});
+    router.get('/decodificarToken',checkToken,(req,res)=>res.send(req.decoded));
 }
--- src/routes/incidentesRota.js
+++ src/routes/incidentesRota.js
@@ -23,4 +23,41 @@ export default function rotearIncidentesRota(router)
 
         })
     });
+
+    router.post('/novoIncidente',(req,res)=>
+    {
+        let tipo = parseInt(req.body.tipo);
+        let local = req.body.local;
+        let informacaoAdicional = req.body.informacaoAdicional;
+        let campos = [local, tipo, informacaoAdicional];
+        let sql = "insert into registrosIncidentes(`local`,`TiposIncidente_idTipoIncidente`,`informacaoAdicional`) values (?,?,?)";
+
+        con.query(sql, campos, (err, result, fields) => {
+            if (err)
+                res.send(err)
+            res.json({sucess:true});
+
+        });
+    });
+
+    router.get('/naoValidados',checkToken,(req,res)=>
+    {
+        let sql = "select * from registrosIncidentes where analisado=0";
+        con.query(sql, (err, result) => {
+            if (err)
+                res.send(err);
+            res.send(result);
+        });
+    })
+
+    router.put('/validaIncidente',checkToken,(req,res)=>
+    {
+        let indices = req.body.indicesReportes;
+                let sql = "update registrosIncidentes set analisado=1 where idRegistrosIncidentes=?";
+                con.query(sql, [indices], (err, result) => {
+                    if (err)
+                        return res.send(err);
+                    res.send(result);
+                })
+    });
 }
